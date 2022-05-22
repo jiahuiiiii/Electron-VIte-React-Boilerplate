fcdeda3cbfb2e9aec441d82b6939e2935f022219
jiahuiiiii <wongjiahui0915@gmail.com>
2022-05-22 19:32:53 +0800

    lel

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,8 +3,11 @@ import './App.css';
 
 const Hello = () => {
   return (
-    <div>
-      <div className=''>hello</div>
+    <div className="bg-amber-200 w-full h-screen flex justify-center items-center flex-col">
+      <div className="text-2xl">Hello</div>
+      <div className="text-base">
+        this is a cute boilerplate for electron with vite and tailwindcss
+      </div>
     </div>
   );
 };
